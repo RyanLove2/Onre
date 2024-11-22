c9982b980bc8dd00a8c4c09beb838297c6aa3098
Ryan <152034200+RyanLove2@users.noreply.github.com>
2024-11-22 14:31:04 -0600

    Add files via upload

--- Onre/Collision.cpp
+++ Onre/Collision.cpp
@@ -26,7 +26,7 @@ bool Collision::Collide(Onre* player, GameObject* enemy){
 void Collision::CollideForGround(Onre* player, vector<GameObject*> ground){
 
 	vector<GameObject*>::iterator it;
-	bool isonfloor;
+
 
 	for(it = ground.begin(); it != ground.end(); ++it ){
 		(*it)->DrawObject();
@@ -56,90 +56,3 @@ void Collision::CollideForGround(Onre* player, vector<GameObject*> ground){
 
 
 	}
-
-
-/*
-
-
-
-
-
-
-	for(it = ground.begin(); it != ground.end(); ++it ){
-
-		//(*it)->DrawObject();
-		if(player->GetLeft() > (*it)->GetLeft()-2 && player->GetRight() < (*it)->GetRight()+5
-		&& player->GetBottom() > (*it)->GetTop() && player->GetTop() < (*it)->GetBottom()){
-
-			isonfloor = false;
-			player->SetFall(isonfloor);
-			//return;
-			break;
-			//std::cout<<"landed: ";
-			//
-		}
-		++it;
-		isonfloor = true;
-
-	}
-	cout<<"ISONFLOOR: "<<isonfloor;
-
-	player->SetFall(isonfloor);
-	return;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-*/
-//top,bottom, left,right;
-//al_draw_filled_rectangle(left, top, right, bottom, al_map_rgb(0,0,200));
-	//	bool finalground = true;
-/*
-		std::cout<<"Player Left: "<<player->GetLeft()<<std::endl;
-		std::cout<<"Ground Left: " <<ground->GetLeft()<<std::endl;
-		std::cout<<"Ground Right: " <<ground->GetRight()<<std::endl;
-		std::cout<<"Player Right: " <<player->GetRight()<<std::endl;
-
-
-	bool finalground;
-
-	if(player->GetLeft() > ground->GetLeft()-2 && player->GetRight() < ground->GetRight()+5
-				&& player->GetBottom() > ground->GetTop() && player->GetTop() < ground->GetBottom()){
-			//	std::cout<<"Right and left is greater then other one"<<std::endl;
-			//	player->SetFall(false);
-				finalground = false;
-				player->SetFall(false);
-				player->onground = true;
-			//cout<<"On Ground "<<endl;
-				//return finalground;
-			}
-			else{
-				//player->SetFall(true);
-			//	player->onground = false;
-				player->onground = false;
-
-			}
-
-
-
-
-
-
-	return finalground;
-*/
-
-
-
